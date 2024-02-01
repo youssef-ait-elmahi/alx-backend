190b1e00360e36808894585fd82a497384d483e2
youssef-ait-elmahi <youssefaitelmahi3@gmail.com>
2024-02-01 18:34:00 +0100

    Refactor BasicCache class methods

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -2,6 +2,7 @@
 """ BasicCache module """
 from base_caching import BaseCaching
 
+
 class BasicCache(BaseCaching):
     """ BasicCache class:
     - Inherits from BaseCaching.
@@ -9,14 +10,18 @@ class BasicCache(BaseCaching):
     """
 
     def put(self, key, item):
-        """ Assign to the dictionary self.cache_data the item value for the key key.
+        """
+        Assign to the dictionary self.cache_data
+        the item value for the key key.
         If key or item is None, this method should not do anything.
         """
         if key is not None and item is not None:
             self.cache_data[key] = item
 
     def get(self, key):
-        """ Return the value in self.cache_data linked to key.
-        If key is None or if the key doesn’t exist in self.cache_data, return None.
+        """
+        Return the value in self.cache_data linked to key.
+        If key is None or if the key doesn’t exist in
+        self.cache_data, return None.
         """
         return self.cache_data.get(key) if key is not None else None
